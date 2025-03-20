27a8b81a76daae0149c0a3ce55ee4867256b2fcd
Itsuki <kobayashi_itsuki@macbookair.local>
2025-03-21 08:33:42 +0900

    Add Models

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['user_id'];
+
+    public function elements() {
+        return $this->hasMany(PostElement::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+}
